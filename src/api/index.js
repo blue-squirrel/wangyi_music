a657a96680147957a2de1c3feceb8641f2b897ec
柠檬味儿松鼠 <1440054388@qq.com>
2020-08-05 17:42:38 +0800

    1

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 // 这个JS文件就是专门用于管理请求各种接口地址的
 import Network from './network'
 
-// 封装各种接 口请求
+// 封装各 种接 口请求
 export const getBanner = () => Network.get('banner?type=2')
 export const getPersonalized = () => Network.get('personalized?limit=6')
 export const getNewAlbum = () => Network.get('album/newest')
